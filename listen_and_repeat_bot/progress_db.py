f352dbb55c0d5780ff6a0cfa6707f7c12eeec73c
Matthew <draevich.matvey@gmail.com>
2022-10-22 15:51:30 +0300

    current channel now can be saved for user; made some fixes for returns value to provide more details for user

--- listen_and_repeat_bot/progress_db.py
+++ listen_and_repeat_bot/progress_db.py
@@ -3,6 +3,8 @@ import logging
 """
 progress_db = {
     user_id: {
+        current_channel: channel_id
+        channel_id: value (QuestionQueue)
         channel_id: value (QuestionQueue)
     }
 }
@@ -11,24 +13,30 @@ progress_db = {
 
 
 class ProgressDatabase():
+    
+    CUR_CHANNEL_KEY = "current_channel_id"
 
     def __init__(self, queue_class):
+        self.logger = logging.getLogger(__name__)
+
         self.queue_class = queue_class
         self.progress_db = {}
-        self.logger = logging.getLogger(__name__)
 
 
     def create_user(self, user_id):
         """
         returns:
-            True - if user_id is created
+            True - if user_id was created
             False - if user_id is already presented in database
         """
 
         if user_id in self.progress_db:
             return False
         else:
-            self.progress_db[user_id] = {}
+            self.progress_db[user_id] = {
+                CUR_CHANNEL_KEY: None
+            }
+            self.logger.info(f"{user_id} was created in database")
             return True
 
 
@@ -41,26 +49,36 @@ class ProgressDatabase():
 
         if user_id in self.progress_db:
             self.progress_db.pop(user_id)
+            self.logger.info(f"{user_id} was deleted from database")
             return True
         else:
             return False
 
 
     def create_channel_progress(self, user_id, channel_id):
-        if user_id not in self.progress_db:
-            self.logger.error(f"{user_id} is not presented in database")
-            return False
+        """
+        function returns:
+            False - user_id:channel_id is already presented in database
+            True - user_id:channel_id was created in database
+        """
+
+        self.create_user(user_id)
 
         if channel_id in self.progress_db[user_id]:
-            self.logger.error(f"{channel_id} is already presented in "
-                              f"database, cannot create it")
             return False
 
         self.progress_db[user_id][channel_id] = self.queue_class()
+        self.logger.info(f"{user_id}:{channel_id} was created in database")
         return True
 
 
     def delete_channel_progress(self, user_id, channel_id):
+        """
+        function returns:
+            False - user_id:channel_id is not presented in database
+            False - user_id:channel_id was deleted from database
+        """
+
         if user_id not in self.progress_db:
             return False
 
@@ -68,18 +86,54 @@ class ProgressDatabase():
             return False
 
         self.progress_db[user_id].pop(channel_id)
+        self.logger.info(f"{user_id}:{channel_id} was deleted from database")
         return True
 
 
+    def get_current_channel_of_user(self, user_id):
+        """
+        function returns a tuple:
+            (0, value) - channel_id value for user_id, 
+                         value is None (if user has no channel_id)
+                         otherwise type(value) is str
+            (-1, None) - user_id is absent in database
+        """
+
+        if user_id in self.progress_db:
+            return (0, self.progress_db[user_id][self.CUR_CHANNEL_KEY])
+        else:
+            return (-1, None)
+
+
+    def set_current_channel_of_user(self, user_id, channel_id):
+        """
+        function returns:
+            True - channel_id is saved for user_id
+            False - user_id is absent in database, cannot save channel_id
+        """
+
+        if user_id in self.progress_db:
+            self.progress_db[user_id][self.CUR_CHANNEL_KEY] = channel_id
+            return True
+        else: 
+            return False 
+
+
     def get_channel_progress(self, user_id, channel_id):
+        """
+        function returns a tuple of:
+            (0, value) - successful query, type(value) is ProgressQueue obj
+            (1, None) - user_id is absent in database
+            (2, None) - channel_id for user_id is absent in database 
+        """
+
         if user_id not in self.progress_db:
-            return None
+            return (1, None)
 
         if channel_id not in self.progress_db[user_id]:
-            return None
+            return (2, None)
 
-
-        return self.progress_db[user_id][channel_id]
+        return (0, self.progress_db[user_id][channel_id])
 
 
     def export_to_json(self):
