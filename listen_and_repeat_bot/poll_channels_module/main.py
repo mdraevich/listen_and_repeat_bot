5ff18523d29dd22500929deae051d824f60fc2c9
Matthew <draevich.matvey@gmail.com>
2022-10-23 23:11:18 +0300

    separated channel polling and telegram bot; implemented async channel polling; publish polling results on async http server

---
+++ listen_and_repeat_bot/poll_channels_module/main.py
@@ -0,0 +1,149 @@
+import os
+import yaml
+import getpass
+import asyncio
+import logging
+
+from aiohttp import web
+from telethon.tl.patched import Message 
+
+from poll_public_channel import PollPublicChannel
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+routes = web.RouteTableDef()
+
+@routes.get('/data')
+async def index_page(request):
+    data = {"channels": []}
+
+    for channel_opts in config["channels"]:
+        posts = [ 
+            post.message
+            for post in poll_controller.get_channel_posts(
+                        channel_opts["channel_id"])
+            if isinstance(post, Message) 
+        ]
+        channel_data = {
+            **channel_opts,
+            "data": posts
+        }
+        data["channels"].append(channel_data)
+
+    return web.json_response(data)
+
+@routes.get('/status')
+async def status(request):
+    data = {"alive": "true"}
+    return web.json_response(data)
+
+
+async def run_http_server(routes, host, port):
+    app = web.Application()
+    app.add_routes(routes)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    site = web.TCPSite(runner, host, port)
+    await site.start()
+
+    while True:
+        await asyncio.sleep(3)
+
+
+async def poll_channel_forever(poll_controller, config):
+    while True:
+        channel_id = config["channel_id"]
+        message_limit = config["message_limit"]
+        polling_interval = config["polling_interval"]
+        
+        await poll_controller.poll_channel(channel_id, message_limit)
+        await asyncio.sleep(polling_interval)
+
+
+def parse_listen_address(listen_address):
+    if listen_address is None:
+        return None, None
+    items = listen_address.split(":")
+    if len(items) == 1:
+        return items[0], None
+    elif len(items) == 2:
+        return items[0], items[1]
+    else:
+        raise ValueError
+
+
+def parse_config_file(filename):
+    try:
+        with open(filename, "r") as file:
+            return yaml.safe_load(file)
+    except (yaml.YAMLError, OSError) as e:
+        if isinstance(e, OSError):
+            print("Cannot read configuration file, "
+                  "check path and permissions")
+        if isinstance(e, yaml.YAMLError):
+            print("Config file has incorrect format, "
+                  "cannot parse config options")
+        return None
+
+
+def create_poll_channel_controller(phone, api_id, api_hash):
+    poll_channel = PollPublicChannel()
+
+    # perform authentication
+    auth_response = poll_channel.authenticate(
+                        phone=phone,api_id=api_id, api_hash=api_hash)
+    auth_code = auth_response[0]
+    if auth_code == 0:  # already authenticated 
+        pass
+    elif auth_code == 1:  # phone confirmation is needed
+        phone_auth_response = poll_channel.confirm_phone(
+                                    phone, code=input("Enter code: "))
+        phone_auth_code = phone_auth_response[0]
+        if phone_auth_code == 0:  # authenticated, success
+            pass
+        elif phone_auth_code == 1:  # cloud password is needed
+            poll_channel.confirm_cloud_password(
+                password=getpass.getpass("Enter 2FA password: "))
+        elif phone_auth_code == 10:  # phone is not registered
+            return None
+    elif auth_code == 5:  # flood error
+        return None
+    return poll_channel
+
+
+if __name__ == '__main__':
+    phone = os.environ.get("PHONE", None)
+    api_id = os.environ.get("API_ID", None)
+    api_hash = os.environ.get("API_HASH", None)
+    listen_address = os.environ.get("LISTEN_ADDRESS", None)
+    host, port = parse_listen_address(listen_address)
+
+    config = parse_config_file(
+             os.environ.get("CONFIG_FILE", "config.yml"))
+    if config is None:
+        print("No correct config found, exiting")
+        exit(1)
+
+    poll_controller = create_poll_channel_controller(
+        phone=phone,
+        api_id=api_id,
+        api_hash=api_hash
+    )
+
+    loop = asyncio.get_event_loop()
+    loop.create_task(run_http_server(routes, host, port))
+    for channel_opts in config["channels"]:
+        loop.create_task(
+            poll_channel_forever(poll_controller, channel_opts))
+    
+    try:
+        loop.run_forever()
+    except KeyboardInterrupt:
+        loop.stop()
---
+++ listen_and_repeat_bot/poll_channels_module/poll_public_channel.py
@@ -0,0 +1,131 @@
+import os
+import time
+import logging
+
+from telethon.sync import TelegramClient
+from telethon import errors
+
+
+
+class PollPublicChannel():
+
+
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+
+        self.channels = {}
+        self.client = None
+
+
+    def authenticate(self, phone, api_id, api_hash):
+        """
+        function accepts args:
+            phone - id for your account
+            api_id - see at https://core.telegram.org/api/obtaining_api_id
+            api_hash - see at https://core.telegram.org/api/obtaining_api_id
+        
+        function returns a tuple:
+            (0) - user is authenticated using cache
+            (1) - user has to confirm phone by submitting code
+            (5, seconds: int) - user has to wait for <seconds>
+                                   before sending a new request 
+        """
+
+        if isinstance(self.client, TelegramClient) and\
+                      self.client.is_connected():
+           self.client.disconnect()
+
+        self.client = TelegramClient(phone, api_id, api_hash)
+
+        try:
+            self.client.connect()
+            
+            if self.client.is_user_authorized():
+                self.logger.info(f"User {phone} is authenticated using "
+                                 f"cache: @{self.client.get_me().username}")
+                return (0,)
+            else:
+                self.logger.info(f"Send confirmation code to {phone}")
+                self.client.send_code_request(phone)
+                return (1,)
+
+        except errors.FloodWaitError as e:
+            self.logger.exception(f"Too much requests, wait for "
+                                  f"{e.seconds} second(s)!")
+            return (5, e.seconds)
+
+
+    def confirm_phone(self, phone, code):
+        """
+            function returns a tuple:
+                (0) - code is correct and user is authorized
+                (1) - 2FA authorization is enabled, provide password 
+                        via PollPublicChannel.confirm_cloud_password method
+                (10) - cannot sign in using specified phone number
+        """
+
+        try:
+            self.client.sign_in(phone, code)
+            self.logger.info(f"Successfully signed in using phone={phone}")
+            return (0,)
+
+        except (errors.SessionPasswordNeededError,
+                errors.PhoneNumberUnoccupiedError) as e:
+            if isinstance(e, errors.SessionPasswordNeededError):
+                self.logger.info("Password is required to "
+                                 "complete authorization")
+                return (1,)
+
+            if isinstance(e, errors.PhoneNumberUnoccupiedError):
+                self.logger.exception(f"Cannot sign in using phone={phone}, "
+                                      f"phone number is not registered")
+                return (10,)
+
+
+    def confirm_cloud_password(self, password):
+        self.client.sign_in(password=password)
+        self.logger.info(f"Successfully signed in")
+        return 0
+
+
+    def get_channel_posts(self, channel_id):
+        """
+        function returns:
+            telethon.helpers.TotalList - messages for channel_id
+            None - if no data is found for channel_id
+        """
+
+        if channel_id in self.channels:
+            return self.channels[channel_id]
+        else:
+            return None
+
+
+    async def poll_channel(self, channel_id, message_limit=100):
+        """
+        function returns a tuple:
+            (0,) - polling is successfull
+            (1,) - connection error during polling
+            (5, seconds: int) - flood error, wait for <seconds>
+        """
+        try:
+            self.channels[channel_id] = await self.client.get_messages(
+                                        channel_id,
+                                        limit=message_limit)
+
+            self.logger.info(f"@{channel_id} has been polled "
+                             f"successfully, got "
+                             f"{len(self.channels[channel_id])} posts")
+            return (0,)
+
+        except (ConnectionError, errors.FloodWaitError) as e:
+            
+            if isinstance(e, ConnectionError):
+                self.logger.error("Network conntection Error, "
+                                  "no messages received")
+                return (1,)
+
+            if isinstance(e, errors.FloodWaitError):
+                self.logger.exception(f"Too much requests, wait for "
+                                      f"{e.seconds} second(s)!")
+                return (5, e.seconds)
