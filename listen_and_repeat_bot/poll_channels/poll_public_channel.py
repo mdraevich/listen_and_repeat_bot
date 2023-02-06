633059e8230f778d71df315eee8a2d6ea49c849a
Matthew <draevich.matvey@gmail.com>
2023-02-07 01:52:09 +0300

    removed logic to perform interactive authentication; changed filename for session file

--- listen_and_repeat_bot/poll_channels/poll_public_channel.py
+++ listen_and_repeat_bot/poll_channels/poll_public_channel.py
@@ -15,81 +15,39 @@ class PollPublicChannel():
         self.client = None
 
 
-    def authenticate(self, phone, api_id, api_hash):
+    def authenticate(self, session_filename, api_id, api_hash):
         """
         function accepts args:
-            phone - id for your account
-            api_id - see at https://core.telegram.org/api/obtaining_api_id
-            api_hash - see at https://core.telegram.org/api/obtaining_api_id
+            session_filename - session filename for your account
+            api_id           - see at https://core.telegram.org/api/obtaining_api_id
+            api_hash         - see at https://core.telegram.org/api/obtaining_api_id
 
         function returns a tuple:
-            (0) - user is authenticated using cache
-            (1) - user has to confirm phone by submitting code
+            (0)               - user is authenticated using cache
+            (1)               - user has to confirm phone by submitting code
             (5, seconds: int) - user has to wait for <seconds>
-                                   before sending a new request
+                                before sending a new request
         """
 
-        self.client = TelegramClient(phone, api_id, api_hash)
+        self.client = TelegramClient(session_filename, api_id, api_hash)
+        self.client.connect()
 
-        try:
-            self.client.connect()
-
-            if self.client.is_user_authorized():
-                self.logger.info("User %s is authenticated using "
-                                 "cache: %s",
-                                 phone, self.client.get_me().username)
-                return (0,)
-
-            self.logger.info("Send confirmation code to %s", phone)
-            self.client.send_code_request(phone)
-            return (1,)
-
-        except errors.FloodWaitError as exc:
-            self.logger.exception("Too much requests, wait for "
-                                  "%s second(s)!", exc.seconds)
-            return (5, exc.seconds)
-
-
-    def confirm_phone(self, phone, code):
-        """
-            function returns a tuple:
-                (0) - code is correct and user is authorized
-                (1) - 2FA authorization is enabled, provide password
-                        via PollPublicChannel.confirm_cloud_password method
-                (10) - cannot sign in using specified phone number
-        """
-
-        try:
-            self.client.sign_in(phone, code)
-            self.logger.info("Successfully signed in using phone=%s", phone)
+        if self.client.is_user_authorized():
+            self.logger.info("Successfully authenticated "
+                             "using session file")
             return (0,)
 
-        except (errors.SessionPasswordNeededError,
-                errors.PhoneNumberUnoccupiedError) as exc:
-            if isinstance(exc, errors.SessionPasswordNeededError):
-                self.logger.info("Password is required to "
-                                 "complete authorization")
-                return (1,)
-
-            if isinstance(exc, errors.PhoneNumberUnoccupiedError):
-                self.logger.exception("Cannot sign in using phone=%s, "
-                                      "phone number is not registered", phone)
-                return (10,)
-
-        return (None,)
+        self.logger.info("Cannot sign in using provided session file")
+        return (1,)
 
 
-    def confirm_cloud_password(self, password):
-        self.client.sign_in(password=password)
-        self.logger.info("Successfully signed in")
-        return (0,)
 
 
     def get_channel_posts(self, channel_id):
         """
         function returns:
             telethon.helpers.TotalList - messages for channel_id
-            None - if no data is found for channel_id
+            None                       - if no data is found for channel_id
         """
 
         if channel_id in self.channels:
