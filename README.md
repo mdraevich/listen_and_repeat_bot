57d9f23283e2eda3091be040215bb0ff3ad892d7
Matthew <draevich.matvey@gmail.com>
2023-02-07 00:08:35 +0300

    add separate authenticate module for creating session files

--- README.md
+++ README.md
@@ -5,3 +5,32 @@
 3. add feature to add user-defined answers for questions.
 4. add feature to define different parsers for different channel.
 5. add more unit tests before building; add unit tests after building.
+
+### Create authentication file
+
+1. Prepare `./secrets` directory if it doesn't exist:
+```shell
+# in the project root directory
+
+mkdir -p secrets
+```
+
+
+2. Prepare environment file with required parameters:
+```shell
+# in the project root directory
+
+cd session_handler/
+echo "API_ID=$API_ID" > .env
+echo "API_HASH=$API_HASH" >> .env
+echo "SESSION_NAME=$SESSION_NAME" >> .env
+```
+
+3. Follow the authentication process to create a session file:
+```shell
+# in the project root directory
+
+cd session_handler/
+pipenv install
+pipenv run python3 main.py
+```
---
+++ session_handler/main.py
@@ -0,0 +1,72 @@
+"""
+creates a session file in ../secrets directory 
+to let you mount the file to a docker container
+"""
+
+import os
+import getpass
+
+from telegram_authenticate import TelegramAuthenticate
+
+
+def interactive_authentication():
+    """
+    provides an interactive way of telegram authentication
+    """
+
+    auth_handler = TelegramAuthenticate()
+
+    # perform authentication
+    return_code = auth_handler.authenticate(
+                        phone=phone,
+                        api_id=api_id,
+                        api_hash=api_hash,
+                        filename=session_filename,
+                        session_name=session_name)
+
+    if return_code[0] == 0:
+        # already authenticated
+        print("Already authenticated, exiting...")
+
+    elif return_code[0] == 1:
+        # phone confirmation is needed
+        confirm_phone_code = auth_handler.confirm_phone(
+                                    phone, code=input("Enter code: "))
+
+        if confirm_phone_code[0] == 0:
+            # authenticated, success
+            print("Authenticated successfully, exiting...")
+
+        elif confirm_phone_code[0] == 1:
+            # cloud password is needed
+            confirm_cloud_pass = auth_handler.confirm_cloud_password(
+                                    getpass.getpass("Enter 2FA password: "))
+
+            if confirm_cloud_pass[0] == 0:
+                # authenticated with 2FA, success
+                print("Authenticated successfully, exiting...")
+
+            else:
+                # 2FA pass authentication is failed
+                print("Authentication failure, exiting...")
+
+        elif confirm_phone_code[0] == 10:
+            # phone is not registered
+            print("The specified phone is not registered.")
+
+    elif return_code[0] == 5:
+        # flood error
+        print(f"FloodWaitError exception is raised, "
+              f"wait for {return_code[1]} seconds.")
+
+
+if __name__ == '__main__':
+    phone = input("Enter phone: ")
+
+    api_id = os.environ.get("API_ID", None)
+    api_hash = os.environ.get("API_HASH", None)
+    session_name = os.environ.get("SESSION_NAME", "Listen & Repeat")
+
+    session_filename = f"../secrets/{phone}.session"
+
+    interactive_authentication()
